a154007252120011aa7145579be2117244d25f3d
yasinshaw <yasinshaw@gmail.com>
2019-04-25 19:54:40 +0800

    add ExtendWith in dao

--- src/test/java/com/yasinshaw/spring_test_demo/repository/UserRepositoryTest.java
+++ src/test/java/com/yasinshaw/spring_test_demo/repository/UserRepositoryTest.java
@@ -2,13 +2,16 @@ package com.yasinshaw.spring_test_demo.repository;
 
 import com.yasinshaw.spring_test_demo.entity.User;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
 @DataJpaTest
 class UserRepositoryTest {
     @Autowired
